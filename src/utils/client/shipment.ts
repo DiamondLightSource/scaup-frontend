3753f11a4743d5cd92e3aca836bc406a45405d33
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-03-21 13:10:27 +0000

    Fix shipment not being updated when changing pages

--- src/utils/client/shipment.ts
+++ src/utils/client/shipment.ts
@@ -4,9 +4,7 @@ export const getShipmentData = async (
   shipmentId: string,
   suffix: string = "",
 ): Promise<Record<string, any> | null> => {
-  const res = await authenticatedFetch.server(`/shipments/${shipmentId}${suffix}`, {
-    next: { tags: [`shipment-${shipmentId}`] },
-  });
+  const res = await authenticatedFetch.server(`/shipments/${shipmentId}${suffix}`);
 
   return res && res.status === 200 ? await res.json() : null;
 };
