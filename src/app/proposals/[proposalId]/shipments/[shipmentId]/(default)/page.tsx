616ffb655e0c86ad7a8c526c5a7326ddabc3b6de
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-03-22 09:43:55 +0000

    Add location/parent to sample listing on shipment page

--- src/app/proposals/[proposalId]/shipments/[shipmentId]/(default)/page.tsx
+++ src/app/proposals/[proposalId]/shipments/[shipmentId]/(default)/page.tsx
@@ -2,6 +2,16 @@ import { components } from "@/types/schema";
 import { authenticatedFetch } from "@/utils/client";
 import { pascalToSpace } from "@/utils/generic";
 import { recursiveCountTypeInstances } from "@/utils/tree";
+import {
+  Divider,
+  HStack,
+  Heading,
+  Stat,
+  StatLabel,
+  StatNumber,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { Metadata } from "next";
 import ShipmentHomeContent from "./pageContent";
 
@@ -13,7 +23,11 @@ const getShipmentData = async (shipmentId: string) => {
   const res = await authenticatedFetch.server(`/shipments/${shipmentId}`);
   const resSamples = await authenticatedFetch.server(`/shipments/${shipmentId}/samples`);
   const data: components["schemas"]["ShipmentChildren"] =
-    res && res.status === 200 ? await res.json() : [];
+    res && res.status === 200 ? await res.json() : null;
+
+  if (data === null) {
+    return data;
+  }
 
   let counts: Record<string, number> = {};
 
@@ -32,7 +46,39 @@ const getShipmentData = async (shipmentId: string) => {
 const ShipmentHome = async ({ params }: { params: { shipmentId: string; proposalId: string } }) => {
   const shipmentData = await getShipmentData(params.shipmentId);
 
-  return <ShipmentHomeContent params={params} data={shipmentData} />;
+  return (
+    <VStack alignItems='start'>
+      <VStack gap='0' alignItems='start' w='100%'>
+        <Heading size='md' color='gray.600'>
+          Shipment
+        </Heading>
+        <Heading>{shipmentData ? shipmentData.name : "Shipment"}</Heading>
+        <Divider borderColor='gray.800' />
+      </VStack>
+      {shipmentData === null ? (
+        <VStack w='100%' mt='3em'>
+          <Heading variant='notFound'>Shipment Unavailable</Heading>
+          <Text>This shipment does not exist or you do not have permission to view it.</Text>
+        </VStack>
+      ) : (
+        <>
+          <HStack w='100%' mb='1em'>
+            <Stat borderBottom='3px solid' borderColor='diamond.700'>
+              <StatLabel>Status</StatLabel>
+              <StatNumber>{shipmentData.dispatch.status || "Unknown"}</StatNumber>
+            </Stat>
+            {Object.entries(shipmentData.counts).map(([key, value]) => (
+              <Stat borderBottom='3px solid' borderColor='diamond.700' key={key}>
+                <StatLabel>{key}</StatLabel>
+                <StatNumber>{value}</StatNumber>
+              </Stat>
+            ))}
+          </HStack>
+          <ShipmentHomeContent params={params} data={shipmentData} />
+        </>
+      )}
+    </VStack>
+  );
 };
 
 export default ShipmentHome;
