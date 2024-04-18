35b3c3b2951213ce24bb3e9082c049c2a8cac691
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-04-18 15:47:37 +0100

    Disable edit button if shipment is booked

--- src/app/proposals/[proposalId]/shipments/[shipmentId]/(view)/[itemType]/[itemId]/layout.tsx
+++ src/app/proposals/[proposalId]/shipments/[shipmentId]/(view)/[itemType]/[itemId]/layout.tsx
@@ -1,5 +1,7 @@
 import { BasePage } from "@/mappings/pages";
 import { ItemParams } from "@/types/generic";
+import { components } from "@/types/schema";
+import { getShipmentData } from "@/utils/client/shipment";
 import ShipmentsLayoutContent from "./layoutContent";
 
 export interface ShipmentsLayoutProps {
@@ -8,7 +10,16 @@ export interface ShipmentsLayoutProps {
 }
 
 const ShipmentsLayout = async ({ children, params }: ShipmentsLayoutProps) => {
-  return <ShipmentsLayoutContent params={params}>{children}</ShipmentsLayoutContent>;
+  const data = (await getShipmentData(
+    params.shipmentId,
+  )) as components["schemas"]["ShipmentChildren"];
+  const isBooked = data && data.data.status === "Booked";
+
+  return (
+    <ShipmentsLayoutContent params={params} isBooked={isBooked}>
+      {children}
+    </ShipmentsLayoutContent>
+  );
 };
 
 export default ShipmentsLayout;
