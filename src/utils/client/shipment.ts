c9bb2378d3ad2d7dfc3ad803b7963fda3d660ba0
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2024-04-26 16:40:05 +0100

    Display correct list of samples

--- src/utils/client/shipment.ts
+++ src/utils/client/shipment.ts
@@ -4,7 +4,9 @@ export const getShipmentData = async (
   shipmentId: string,
   suffix: string = "",
 ): Promise<Record<string, any> | null> => {
-  const res = await authenticatedFetch.server(`/shipments/${shipmentId}${suffix}`);
+  const res = await authenticatedFetch.server(`/shipments/${shipmentId}${suffix}`, {
+    cache: "no-store",
+  });
 
   return res && res.status === 200 ? await res.json() : null;
 };
