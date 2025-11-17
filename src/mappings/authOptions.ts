482baebea38db6f34117d07c97fedc39783eba0f
Guilherme de Freitas <guilherme.de-freitas@diamond.ac.uk>
2025-11-17 09:09:55 +0000

    Change which ID is displayed when Keycloak is used

--- src/mappings/authOptions.ts
+++ src/mappings/authOptions.ts
@@ -13,13 +13,15 @@ export const authOptions: NextAuthOptions = {
     },
   },
   session: {
-    maxAge: 60 * 60 * 6,
+    maxAge: 60 * 3,
   },
   providers: [
     {
+      authorization: {params: {scope: "openid profile email fedid"}},
       id: "diamond",
       name: "Diamond",
       type: "oauth",
+      idToken: true,
       clientId: process.env.OAUTH_CLIENT_ID,
       clientSecret: process.env.OAUTH_CLIENT_SECRET,
       wellKnown: process.env.OAUTH_DISCOVERY_ENDPOINT,
@@ -31,10 +33,10 @@ export const authOptions: NextAuthOptions = {
 
         const user = await response.json();
         return {
-          id: profile.sub,
-          email: profile.sub,
+          id: profile.fedid,
+          email: profile.email,
           permissions: user.permissions,
-          name: user.givenName,
+          name: profile.given_name,
           accessToken: tokens.access_token,
         };
       },
